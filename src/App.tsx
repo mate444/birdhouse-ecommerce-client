c8864ffcf8b6d43defbc824251f197eb79bd61e4
mate444 <mateoeo23@gmail.com>
2023-06-04 12:41:30 +0200

    Adds Component Routing + default image asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Home from "./components/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route element={<Home />} path="/"/>
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Routes>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ChakraProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
+    </RecoilRoot>
   </React.StrictMode>,
 );
