9b4dae3b6c4deaeab569ffaa7f31476fca4707d3
mate444 <mateoeo23@gmail.com>
2023-06-09 18:03:17 +0200

    Adds hook useError

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,13 +4,8 @@ enum HttpMethod {
   DELETE = "delete",
   GET = "get",
   PATCH = "patch",
-  POST = "put",
+  POST = "post",
   PUT = "put",
-}
-
-interface RequestError {
-  status: number,
-  message: string
 }
 
 export const useFetch = () => {
@@ -22,40 +17,11 @@ export const useFetch = () => {
     putt: request(HttpMethod.PUT)
   };
   function request (method: HttpMethod) {
-    return async (url?: string) => {
-      try {
-        if (typeof url !== "string") throw new TypeError("URL Method must be a string");
-        const response = await axios[method](url);
-        const { data, status } = response;
-        if (status !== 201 && status !== 200) errorRequest({ status, message: data });
-        return data;
-      } catch (err) {
-        console.log(err);
-      }
+    return async (url?: string, body?: any) => {
+      if (typeof url !== "string") throw new TypeError("URL Method must be a string");
+      const response = await axios[method](url, body);
+      const { data } = response;
+      return data;
     };
-  }
-  function errorRequest (error: RequestError) {
-    switch (error.status) {
-    case 500:
-      return "Something went wrong";
-    
-    case 400:
-      return error.message;
-
-    case 401:
-      return error.status;
-
-    case 403:
-      return error.status;
-    
-    case 429:
-      return {
-        error: error.status,
-        // Add how much time does to user have to wait
-        message: "You have sent too many requests please wait"
-      };
-    default:
-      return "Something went wrong";
-    }
   }
 };
