4dba6d7be5db931e3437ac44e9c5981fb1c19369
mate444 <mateoeo23@gmail.com>
2023-07-03 20:12:14 +0200

    Adds Birdhouse Card styling

--- src/components/BirdhouseCard/BirdhouseCard.tsx
+++ src/components/BirdhouseCard/BirdhouseCard.tsx
@@ -1,7 +1,6 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import defaultBirdhousePicture from "../../assets/default_birdhouse.jpg";
 import {
-  Box,
   Button,
   Card,
   CardBody,
@@ -9,7 +8,7 @@ import {
   Heading,
   Image,
   Text,
-  Stack
+  Stack,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useCartActions } from "../../actions/cart.actions";
@@ -25,43 +24,43 @@ interface BirdhouseCardProps {
 const BirdhouseCard: FC<BirdhouseCardProps> = (props) => {
   const { addItem } = useCartActions();
   return (
-    <Box 
-      // w={["100vw", "100vw", "30vw", "20vw", "20vw"]}
-    >
-      <Card bg="white">
-        <CardBody>
+    <Card w={["100%", "100%", "100%", "30vw", "20vw"]} border={"1px"} borderColor={"#989898"} size={"sm"} bg="white">
+      <CardBody p="0">
+        <Link to={`/birdhouse/${props.id}`}>
+          <Image
+            src={props.picture}
+            borderRadius={2}
+            fallbackSrc={defaultBirdhousePicture}
+            alt={"birdhouse_image"}
+            w={"100%"}
+            h={["40vh", "35vh", null, null, "40vh"]}
+          />
+        </Link>
+        <Stack mx="1vw" mt='6'>
           <Link to={`/birdhouse/${props.id}`}>
-            <Image
-              src={props.picture}
-              fallbackSrc={defaultBirdhousePicture}
-              alt={"birdhouse_image"}
-              // borderRadius="base"
-              // boxSize={"fit-content"}
-            />
+            <Heading size='md'>{props.name}</Heading>
           </Link>
-          <Stack mt='6' spacing='3'>
-            <Link to={`/birdhouse/${props.id}`}>
-              <Heading size='md'>{props.name}</Heading>
-            </Link>
-            <Text color='1B9706' fontSize='2xl'>
-              {props.price}€
-            </Text>
-          </Stack>
-        </CardBody>
-        <CardFooter>
-          <Button onClick={() => addItem({
-            birdhouseId: props.id,
-            name: props.name,
-            price: props.price,
-            picture: props.picture,
-            stock: props.stock,
-            quantity: 1
-          })}>
-            Add to cart
-          </Button>
-        </CardFooter>
-      </Card>
-    </Box>
+          <Text color='1B9706' fontSize='2xl'>
+            {props.price}€
+          </Text>
+        </Stack>
+      </CardBody>
+      <CardFooter>
+        <Button onClick={() => addItem({
+          birdhouseId: props.id,
+          name: props.name,
+          price: props.price,
+          picture: props.picture,
+          stock: props.stock,
+          quantity: 1
+        })}
+        bgColor="#1B9706"
+        color="white"
+        fontWeight={"light"}>
+          Add to cart
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
