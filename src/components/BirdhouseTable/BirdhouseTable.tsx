126103d87f9660b77c14b7b2059393424bfc0144
mate444 <mateoeo23@gmail.com>
2023-07-10 11:05:15 +0200

    Adds Sort and pagination to birdhouses Table

--- src/components/BirdhouseTable/BirdhouseTable.tsx
+++ src/components/BirdhouseTable/BirdhouseTable.tsx
@@ -6,11 +6,15 @@ import {
   Tbody,
   Tr,
   Th,
-  useToast
+  useToast,
+  Link,
+  Flex
 } from "@chakra-ui/react";
+import Pagination from "../Pagination/Pagination";
 import BirdhouseCell from "../BirdhouseCell/BirdhouseCell";
+import SortSelect from "../SortSelect/SortSelect";
 import { useBirdhouseActions } from "../../actions/birdhouse.actions";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link as ReactLink } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { birdhousesAtom } from "../../states/birdhouse";
 import { BirdhouseInterface } from "../../interfaces/Birdhouse.interface";
@@ -37,47 +41,65 @@ const columns = [{
 
 const BirdhouseTable: FC = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [queryParams, setQueryParams] = useSearchParams();
+  const [queryParams] = useSearchParams();
   const toast = useToast();
-  const { getAll, update, remove } = useBirdhouseActions(toast);
+  const { getAll, update, remove, setSort } = useBirdhouseActions(toast);
   const birdhouseState = useRecoilValue(birdhousesAtom);
   let page = queryParams.get("page");
   useEffect(() => {
     if (page === null) page = "1";
-    getAll(page, birdhouseState.birdhouseSearch).then(() => {
+    getAll(page, birdhouseState.birdhouseSearch, birdhouseState.birdhouseSort).then(() => {
       setIsLoading(false);
+      window.scrollTo(0, 0);
     });
-  }, []);
+  }, [page, birdhouseState.birdhouseSort]);
   return (
-    !isLoading ? <Table variant="simple">
-      <Thead>
-        <Tr>
-          { columns.map((c) => (
-            <Th key={c.text} isNumeric={c.isNumeric}>
-              {c.text}
-            </Th>
-          )) }
-        </Tr>
-      </Thead>
-      <Tbody>
-        {  birdhouseState.birdhouses.map((b: BirdhouseInterface) => (
-          <BirdhouseCell
-            key={b.birdhouseId}
-            birdhouseId={b.birdhouseId}
-            update={update}
-            remove={remove}
-            description={b.description}
-            name={b.name}
-            price={`${b.price}`}
-            size={`${b.size}`}
-            stock={`${b.stock}`}
-            styles={b.styles}
-            pictures={b.pictures}
-            status={b.status}
-          />
-        )) }
-      </Tbody>
-    </Table>
+    !isLoading ?
+      <>
+        <Link as={ReactLink} to="/birdhouse/create">
+          <Flex
+            m="10px"
+            bgColor={"#1B9706"}
+            color="white"
+            width="25%"
+            justify="center"
+            borderRadius={20}
+            p="10px">
+            Create a Birdhouse
+          </Flex>
+        </Link>
+        <SortSelect setSort={setSort} route={`/admin/birdhouses?page=1`}/>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              { columns.map((c) => (
+                <Th key={c.text} isNumeric={c.isNumeric}>
+                  {c.text}
+                </Th>
+              )) }
+            </Tr>
+          </Thead>
+          <Tbody>
+            {  birdhouseState.birdhouses.map((b: BirdhouseInterface) => (
+              <BirdhouseCell
+                key={b.birdhouseId}
+                birdhouseId={b.birdhouseId}
+                update={update}
+                remove={remove}
+                description={b.description}
+                name={b.name}
+                price={`${b.price}`}
+                size={`${b.size}`}
+                stock={`${b.stock}`}
+                styles={b.styles}
+                pictures={b.pictures}
+                status={b.status}
+              />
+            )) }
+          </Tbody>
+        </Table>
+        <Pagination route="/admin/birdhouses" totalPages={parseInt(birdhouseState.totalPages || "1")} currentPage={parseInt(page || "1")} sort={birdhouseState.birdhouseSort}/>
+      </>
       : <CircularProgress isIndeterminate/>);
 };
 
