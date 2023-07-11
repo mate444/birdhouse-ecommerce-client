3f1b2033d0e4a585250f1a497a647d7612097f6a
mate444 <mateoeo23@gmail.com>
2023-07-11 16:11:24 +0200

    Adds styling to Register component

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -4,20 +4,19 @@ import {
   Box,
   Button,
   FormControl,
-  FormLabel,
   FormErrorMessage,
   Heading,
   Image,
   Input,
-  InputGroup,
-  InputRightElement,
+  IconButton,
   Link,
   Select,
   Tooltip,
-  Text,
   useToast,
+  Flex,
   CircularProgress
 } from "@chakra-ui/react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 import { useForm } from "react-hook-form";
 import { useUserActions } from "../../actions/user.actions";
 import { getCountries } from "../../utils/countries";
@@ -35,62 +34,101 @@ const Register: FC = () => {
     setShowPassword(!showPassword);
   };
   return (
-    <Box>
-      <Box>
-        <Heading>
-          Create your account
-        </Heading>
-        <Image src={BirdPic} alt="Bird Picture"/>
-      </Box>
-      <Box>
-        <form onSubmit={handleSubmit(registerAction)}>
-          <FormControl isInvalid={!!errors.country || !!errors.email || !!errors.password}>
-            <FormLabel>Enter your email</FormLabel>
-            <FormErrorMessage>
-              {errors.email && errors.email.message}
-            </FormErrorMessage>
-            <Input isInvalid={!!errors.email} id="email" {...register("email", {
-              validate: registerValidations.email
-            })} placeholder="example@domain.com"/>
-            <FormLabel>Type your password</FormLabel>
-            <FormErrorMessage>
-              {errors.password && errors.password.message}
-            </FormErrorMessage>
-            <InputGroup>
-              <Input isInvalid={!!errors.password} id="password" type={ showPassword ? "text" : "password" } {...register("password", {
-                validate: registerValidations.password
-              })} placeholder="Type your password"/>
-              <InputRightElement>
-                <Button onClick={handlePasswordVisibility}>
-                  {showPassword ? "Hide" : "Show"}
-                </Button>
-                <Box>
+    <Flex h={"90vh"} overflow={"hidden"} justifyContent={"center"} alignItems={"center"}>
+      <Flex flexDir={"column"} maxW={"500px"}>
+        <Flex flexDir={"column"} gap={5} alignItems={"center"}>
+          <Heading
+            mt={"10px"}
+            p={"10px"}
+            bgColor={"white"}
+            borderRadius={20}
+            textAlign={"center"}>
+            Create your account
+          </Heading>
+          <Image src={BirdPic} borderRadius={5} alt="Bird Picture"/>
+        </Flex>
+        <Flex flexDir={"column"}>
+          <form onSubmit={handleSubmit(registerAction)}>
+            <FormControl
+              p={"10px"}
+              display={"flex"}
+              flexDir={"column"}
+              gap={5}
+              isInvalid={!!errors.country || !!errors.email || !!errors.password}>
+              <FormErrorMessage>
+                {errors.email && errors.email.message}
+              </FormErrorMessage>
+              <Input
+                bgColor={"white"}
+                borderRadius={20}
+                isInvalid={!!errors.email} id="email" {...register("email", {
+                  validate: registerValidations.email
+                })}
+                placeholder="Insert your email"/>
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
+              <Flex>
+                <Input
+                  bgColor={"white"}
+                  autoComplete={"password"}
+                  borderRadius={"20px 0px 0px 20px"}
+                  border={0}
+                  isInvalid={!!errors.password} id="password" type={ showPassword ? "text" : "password" } {...register("password", {
+                    validate: registerValidations.password
+                  })}
+                  placeholder="Insert your password"/>
+                <IconButton
+                  aria-label={"change-password-visibility"}
+                  bgColor={"white"}
+                  borderRadius={0}
+                  onClick={handlePasswordVisibility}
+                  icon={showPassword ? <FaEyeSlash/> : <FaEye />}/>
+                <Flex
+                  alignItems={"center"}
+                  pr={"20px"}
+                  bgColor={"white"}
+                  borderRadius={"0px 20px 20px 0px"}>
                   <Tooltip label="Password must contain at least 1 number, 8 characters and 1 symbol">?</Tooltip>
-                </Box>
-              </InputRightElement>
-            </InputGroup>
-            <FormErrorMessage>
-              {errors.country && errors.country.message}
-            </FormErrorMessage>
-            <Select isInvalid={!!errors.country} id="countries" {...register("country", {
-              validate: registerValidations.country
-            })} placeholder="Select a country">{
-                countries.map((c) => (
-                  <option key={c.code} value={c.name}>{c.name}</option>
-                ))
-              }</Select>
-            <Button disabled={isSubmitting} type="submit">{ isSubmitting ? <CircularProgress isIndeterminate/> : "Create" }</Button>
-          </FormControl>
-        </form>
-      </Box>
-      <Box>
-        <Text>
-          Already have an account?
-          Log in{" "}
-          <Link as={ReactLink} to="/login"> here</Link>
-        </Text>
-      </Box>
-    </Box>
+                </Flex>
+              </Flex>
+              <FormErrorMessage>
+                {errors.country && errors.country.message}
+              </FormErrorMessage>
+              <Select
+                bgColor={"white"}
+                borderRadius={20}
+                isInvalid={!!errors.country} id="countries" {...register("country", {
+                  validate: registerValidations.country
+                })} placeholder="Select a country">{
+                  countries.map((c) => (
+                    <option key={c.code} value={c.name}>{c.name}</option>
+                  ))
+                }</Select>
+              <Button
+                color={"white"}
+                bgColor={"#1B9706"}
+                disabled={isSubmitting}
+                type="submit">{ isSubmitting ? <CircularProgress isIndeterminate/> : "Create" }</Button>
+            </FormControl>
+          </form>
+        </Flex>
+        <Box
+          alignSelf={"center"}
+          maxW={"250px"}
+          textAlign={"center"}
+          bgColor={"white"}
+          p={"20px"}
+          borderRadius={20}
+          m={"5px"}>
+          <Heading size={"xs"}>
+            Already have an account?
+            Log in{" "}
+            <Link textDecor={"underline"} color={"blue"} as={ReactLink} to="/login"> here</Link>
+          </Heading>
+        </Box>
+      </Flex>
+    </Flex>
   );
 };
 
