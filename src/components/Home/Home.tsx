45a0fcbc4df3ce51065e1bddfc6237238ff03e5a
mate444 <mateoeo23@gmail.com>
2023-07-20 16:59:00 +0200

    Adds loader to home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import BirdhouseList from "../BirdhouseList/BirdhouseList";
 import Pagination from "../Pagination/Pagination";
 import SortSelect from "../SortSelect/SortSelect";
@@ -10,21 +10,24 @@ import { Box, CircularProgress, Flex, Heading } from "@chakra-ui/react";
 
 const Home: FC = () => {
   const { getAll, setSort } = useBirdhouseActions();
+    const [isLoading, setIsLoading] = useState(true);
   const birdhouseState = useRecoilValue(birdhousesAtom);
   const [queryParams] = useSearchParams();
   let page = queryParams.get("page");
   useEffect(() => {
     if (page === null) page = "1";
-    getAll(page, birdhouseState.birdhouseSearch, birdhouseState.birdhouseSort)
-      .then(() => {
-        window.scrollTo(0, 0);
-      });
+      getAll(page, birdhouseState.birdhouseSearch, birdhouseState.birdhouseSort)
+          .then(() => {
+              setIsLoading(false);
+              window.scrollTo(0, 0);
+          }).catch(() => setIsLoading(false));
   }, [page, birdhouseState.birdhouseSort, birdhouseState.birdhouseSearch]);
   return (
     <Flex marginX={[0, 0, "10vw"]} flexDirection={"column"}>
-      <Heading mt="1em" alignSelf={"center"}> Birdhouses showcase </Heading>
-      { birdhouseState.birdhouses?.length > 0 ?
+          {isLoading && <CircularProgress alignSelf={"center"} isIndeterminate />}
+      { !isLoading && birdhouseState.birdhouses?.length > 0 ?
         <>
+          <Heading mt="1em" alignSelf={"center"}> Birdhouses showcase </Heading>
           <Box mt="1em" w={["100%", "50%", null, "33%", "25%"]} alignSelf={"flex-end"}>
             <SortSelect setSort={setSort} route={`/?page=1`}/>
           </Box>
@@ -32,7 +35,7 @@ const Home: FC = () => {
           <Box alignSelf={"center"}>
             <Pagination route="/" totalPages={parseInt(birdhouseState.totalPages)} sort={birdhouseState.birdhouseSort} currentPage={parseInt(page || "1")}/>
           </Box>
-        </> : <CircularProgress alignSelf={"center"} isIndeterminate/>}
+              </> : <Heading mt="1em" alignSelf={"center"}> There are no birdhouses currently available </Heading>}
     </Flex>
   );
 };
